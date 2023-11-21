c274a04e7bf8258920f1e22a5a214c10d414f8b8
reza yarahmadi <rezayarjob@gmail.com>
2023-11-21 19:41:43 +0330

    test route

--- src/page3.js
+++ src/page3.js
@@ -2,6 +2,7 @@ import { Link } from "react-router-dom";
 import { useContext } from "react";
 import { foods_database } from "./context";
 import { useParams } from "react-router-dom";
+import { Navigate } from "react-router-dom";
 
 function Page3() {
     let foods = useContext(foods_database);
@@ -9,15 +10,25 @@ function Page3() {
     console.log(params);
     return (
         <>
-            <h1>page3</h1>
-            <Link to="/">go to home</Link>
-            <hr />
-            {foods.allProducts.map(a =>
-                <>
-                    <Link to={`/${a.id}`}>{a.id}</Link>
-                    <hr />
+            {params.op == ':200' ?
+                (<>
+                    <Navigate to='/' />
                 </>
-            )}
+                ) : (
+                    <>
+                        <h1>page3</h1>
+                        <Link to="/">go to home</Link>
+                        <hr />
+                        {foods.allProducts.map(a =>
+                            <>
+                                <Link to={`/${a.id}`}>{a.id}</Link>
+                                <hr />
+                            </>
+                        )}
+                    </>
+                )
+            }
+
         </>
     )
 }
